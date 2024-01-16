f50d59de3e1889a55d91589a723c61a7b17aa2c6
Meb. Mohamed Rida <paytf@outlook.com>
2024-01-16 13:20:45 +0100

    Update main.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import '@fontsource/metropolis';
 import '@fontsource/metropolis/400.css';
+import '@fontsource/metropolis/500.css';
 import '@fontsource/metropolis/700.css';
 import './styles/index.css'
 
